2ad82a518015e6d080d1afec3ae6de0c984a4f83
Adrian Russell <adrian.f.russell@gmail.com>
2022-12-20 11:45:13 +1100

    interim changes

---
+++ src/HighCouplingAPI/Controllers/WindController.cs
@@ -0,0 +1,25 @@
+﻿using HighCouplingAPI.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HighCouplingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WindController : ControllerBase
+    {
+        private readonly IRepositoryBase _repository;
+
+        public WindController(IRepositoryBase repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(long id, TemperatureMeasurement temperatureMeasurement)
+        {
+            await _repository.Create(temperatureMeasurement);
+            return NoContent();
+        }
+    }
+}
+
---
+++ src/HighCouplingAPI/Controllers/WindMeasurement.cs
@@ -0,0 +1,11 @@
+﻿namespace HighCouplingAPI.Controllers;
+
+public class WindMeasurement
+{
+    public long Id { get; set; }
+    public int WindSpeedMph{ get; set; }
+
+    public string WindDirection { get; set; } = null!;
+
+    public string MeasurementLocation { get; set; } = null!;
+}
